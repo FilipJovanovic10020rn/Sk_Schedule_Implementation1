e8f94eef81a39fe48dd4da5471d2646494e00d5d
Vanja Radulovic <vradulovic11220rn@raf.rs>
2023-11-11 21:37:52 +0100

    importCSV

--- src/main/java/rs/raf/Implementation1.java
+++ src/main/java/rs/raf/Implementation1.java
@@ -12,9 +12,9 @@ import rs.raf.schedule_management.ScheduleManager;
 import com.opencsv.CSVWriter;
 
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -310,6 +310,63 @@ public class Implementation1 implements ClassSchedule {
     @Override
     public void importCSV(Schedule schedule, String filePath) {
 
+        int duration;
+        boolean flag = false;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            // Read the file line by line
+            while ((line = br.readLine()) != null) {
+                // Split the line by the CSV delimiter (comma, in this case)
+                String[] fields = line.split(",");
+
+                // Process the fields
+                for (int i = 0; i < fields.length; i++) {
+                    // Remove leading and trailing spaces and quotation marks
+                    //if
+                    fields[i] = fields[i].trim().replaceAll("^\"|\"$", "");
+
+
+                    System.out.print(fields[i] + " ");
+                }
+                if(flag){
+
+                    System.out.println(fields[1]);
+                    System.out.println(fields[0]);
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+                    Date date = dateFormat.parse(fields[3]);
+
+                    String[] start = fields[4].split(":");
+                    String[] end = fields[5].split(":");
+                    int s = Integer.parseInt(start[0]);
+                    int e = Integer.parseInt(end[0]);
+                    duration = e-s;
+
+                    ClassLecture cl = new ClassLecture(fields[0],fields[1],s,duration,date,null);
+
+                    System.out.println(cl.toString());
+                    for(Map.Entry<Term,ClassLecture> entry : schedule.getScheduleMap().entrySet()){
+                        for(int j = 0; j<duration; j++){
+                            if(entry.getKey().getDate().equals(date) && entry.getKey().getClassroom().getName().equals(fields[2])
+                                    && entry.getKey().getStartTime() == s+j)
+                            {
+                                schedule.getScheduleMap().put(entry.getKey(),cl);
+                            }
+                        }
+                    }
+
+                }
+
+                flag=true;
+                System.out.println(); // Move to the next line
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -339,7 +396,6 @@ public class Implementation1 implements ClassSchedule {
         } catch (IOException e) {
             e.printStackTrace(); // Handle the exception according to your application's needs
         }
-
 
     }
 
